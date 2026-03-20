cb72263c549354c90cfd78cc8cb93f77c93be304
Rado <nomenaratovonjanahary@gmail.com>
2026-03-20 13:53:06 +0300

    feat: initial commit TD2

---
+++ td2/src/test/java/org/td2/td2/Td2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.td2.td2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
