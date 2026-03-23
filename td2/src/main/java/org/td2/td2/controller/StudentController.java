0129b3847dd53b4a0524fd489a41a1474c29a3bd
Rado <nomenaratovonjanahary@gmail.com>
2026-03-23 13:21:56 +0300

    feat: reponse B

--- td2/src/main/java/org/td2/td2/controller/StudentController.java
+++ td2/src/main/java/org/td2/td2/controller/StudentController.java
@@ -1,13 +1,27 @@
 package org.td2.td2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.td2.td2.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
+    private final List<Student> students = new ArrayList<>();
     @GetMapping("/welcome")
     public String welcome(@RequestParam("name") String name) {
         return "Welcome " + name;
+    }
+
+    @PostMapping("/students")
+    public String addStudents(@RequestBody List<Student> newStudents) {
+        students.addAll(newStudents);
+        String names = students.stream()
+                .map(s -> s.getFirstName() + " " + s.getLastName())
+                .collect(Collectors.joining(", "));
+
+        return names.isEmpty() ? "Aucun étudiant enregistré" : names;
     }
 }
--- td2/src/main/java/org/td2/td2/model/Student.java
+++ td2/src/main/java/org/td2/td2/model/Student.java
@@ -1,6 +1,11 @@
 package org.td2.td2.model;
 
+import lombok.Data;
 
+@Data
 public class Student {
-
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private int age;
 }
